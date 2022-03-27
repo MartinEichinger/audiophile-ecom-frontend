568fdca091bb5df02089794a5ea8320a3881d67a
EdMadd <martl.eichinger@gmail.com>
2022-03-27 19:52:44 +0200

    init

--- packages/audiophile-theme/src/components/index.js
+++ packages/audiophile-theme/src/components/index.js
@@ -45,10 +45,10 @@ const Root = ({ state }) => {
           rel="stylesheet"
         />
         <link
+          rel="preload"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
+          as="style"
+          onload="this.onload=null;this.rel='stylesheet'"
         />
 
         <link
