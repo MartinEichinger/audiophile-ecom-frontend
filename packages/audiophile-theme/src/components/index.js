f5b37c2fc8d1dcd77551aedf39377557a4de0614
EdMadd <martl.eichinger@gmail.com>
2022-03-27 19:54:01 +0200

    init

--- packages/audiophile-theme/src/components/index.js
+++ packages/audiophile-theme/src/components/index.js
@@ -83,10 +83,12 @@ const Root = ({ state }) => {
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"
+        async
       ></script>
       <script
         src="https://kit.fontawesome.com/cea30e219f.js"
         crossorigin="anonymous"
+        async
       ></script>
     </>
   );
